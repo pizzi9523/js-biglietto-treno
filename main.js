961721e910b7665937f476af42b04c4b42766669
pizzi9523 <pizzitolagiuseppe@live.it>
2021-10-11 15:23:47 +0200

    Inserimento dati da parte dell'utente

---
+++ main.js
@@ -0,0 +1,5 @@
+const numberKm = parseFloat(prompt("Quanti Km vuoi percorrere?"));
+const passengerAge = parseInt(prompt("Quanti anni ha il passeggero"));
+//console.log(numberKm);
+//console.log(passengerAge);
+
