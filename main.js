35c40361489886ada86e4e698141cc9ef43a0674
pizzi9523 <pizzitolagiuseppe@live.it>
2021-10-11 15:48:55 +0200

    Calcolo prezzo del biglietto in tutti i casi

--- main.js
+++ main.js
@@ -3,3 +3,16 @@ const passengerAge = parseInt(prompt("Quanti anni ha il passeggero"));
 //console.log(numberKm);
 //console.log(passengerAge);
 
+let ticketPrice = numberKm * 0.21;
+//console.log(ticketPrice);
+
+if (passengerAge < 18) {
+    ticketPrice = ticketPrice - ((ticketPrice * 20) / 100)
+    //console.log(ticketPrice);
+}
+else if (passengerAge >= 65) {
+    ticketPrice = ticketPrice - ((ticketPrice * 40) / 100)
+    //console.log(ticketPrice);
+}
+
+console.log(`Il prezzo del biglietto è ${ticketPrice.toFixed(2)} €`);
